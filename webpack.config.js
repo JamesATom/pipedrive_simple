337637eb26714293b3af25bd6716183429384529
Abdulaziz <abduhakimovabdulaziz7@gmail.com>
2024-12-20 01:43:09 +0500

    feat: Test a button

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,13 @@
-// webpack.config.js
+// filepath: /d:/Start-Up/js intern/webpack.config.js
+const path = require('path');
+
 module.exports = {
     entry: './index.js',
     output: {
         filename: 'bundle.js',
-        path: __dirname + '/dist'
+        path: path.resolve(__dirname, 'dist')
     },
+    mode: 'production', // Set the mode to 'production'
     module: {
         rules: [
             {
